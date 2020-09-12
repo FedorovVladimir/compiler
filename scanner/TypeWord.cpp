97ba20d72dc2501a3b02201f627e5ca69fd03c60
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2020-09-12 14:42:37 +0700

    add TypeWord.cpp

---
+++ scanner/TypeWord.cpp
@@ -0,0 +1,73 @@
+//
+// Created by 3crabs on 12.09.2020.
+//
+
+#ifndef DEFS_H
+#define DEFS_H
+
+enum TypeWord {
+
+    // типы данных
+    VOID,
+    DOUBLE,
+    INT,
+    BOOL,
+    CHAR,
+    SHORT,
+    LONG,
+
+    // Знаки арифметических операций
+    PLS,
+    MNS,
+    DIV,
+    MOD,
+    MUL,
+    INC,
+    DEC,
+
+    // Ключевые слова
+    WHILE,
+    IF,
+    ELSE,
+    SWITCH,
+    CASE,
+    ARRAY,
+    BREAK,
+    DEFAULT,
+
+    // Идентификатор
+    ID,
+
+    // Знаки сравнений
+    LESS,
+    LARGER,
+    LESS_EQUAL,
+    LARGER_EQUAL,
+    EQUAL,
+    NOT_EQUAL,
+    SAVE,
+
+    // Специальные знаки
+    POINT_COMMA,
+    OPEN_KRUGLAY,
+    CLOSE_KRUGLAY,
+    OPEN_FIGURNAY,
+    CLOSE_FIGURNAY,
+    COMMA,
+    POINT,
+
+    // константы
+    CONST_DEC,
+    CONST_HEX,
+    CONST_BOLL,
+    CONST_CHAR,
+    CONST_STR,
+
+    // Конец файла
+    END_OF_FILE,
+
+    // Ошибочный символ
+    ERROR,
+};
+
+#endif // DEFS_H
