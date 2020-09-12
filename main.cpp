5d80c5428167201a1b3383515e319d172ba1559e
Fedorov Vladimir <vladimir.fodorow@gmail.com>
2020-09-12 15:22:40 +0700

    read text from file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string readCode(const string &path) {
+    ifstream in(path);
+    string text;
+    getline(in, text, '\0');
+    in.close();
+    return text + "\n";
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // чтаем файл
+    string text = readCode("/home/vladimir/CLionProjects/compiler/resources/program.code");
+    if (text == "\n") {
+        cout << "File not found!" << endl;
+        return 0;
+    }
+
+    cout << text << endl;
     return 0;
 }
